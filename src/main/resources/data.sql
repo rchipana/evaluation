327c23bcdb7581863f5416f88e8c90777271e0cb
richard <johndoe@example.com>
2018-09-23 13:29:56 -0500

    kk

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO user (`id`,`name`,`email`,`age`,`phone`) VALUES (null,'Nick Fury','nick.fury@shield.com',50,'001 5623774832');
 INSERT INTO user (`id`,`name`,`email`,`age`,`phone`) VALUES (null,'Phil Coulson','phil.coulson@shield.com',40,'001 56237776352');
 INSERT INTO user (`id`,`name`,`email`,`age`,`phone`) VALUES (null,'Melinda May','melinda.may@shield.com',30,'001 56237993647');
-INSERT INTO user (`id`,`name`,`email`,`age`,`phone`) VALUES (null,'Steve Rogers','stev.rogers@shield.com',30,'001 5623774299');
+INSERT INTO user (`id`,`name`,`email`,`age`,`phone`) VALUES (null,'Steve Rogers','steve.rogers@shield.com',30,'001 5623774299');
